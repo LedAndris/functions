776f1e6789c588f72cb49dfdb632d8bb884797a3
LedAndris <ledererandras2004@gmail.com>
2020-07-28 00:19:01 +0200

    Update functions.lua

--- functions.lua
+++ functions.lua
@@ -287,3 +287,72 @@ if PlayerPedId() ~= exc then
 end
 end)
 
+        --[[
+        ####################################################################################################################################################################################################################
+        ####################################################################################################################################################################################################################
+        ####################################################################################################################################################################################################################
+              ]]--
+function reverse(tbl)
+  local ret = {}
+  table.sort(tbl)
+  for i=1, #tbl do
+     table.insert(ret, tbl[#tbl + 1 - i])
+  end
+  return ret
+  end
+
+
+       
+  function drawTxt(x, y ,width, height, text, r,g,b,a, font)
+    SetTextFont(0) -- 0-4
+    SetTextScale(width, height) -- Size of text
+    SetTextColour(r, g, b, a) -- RGBA
+    SetTextEntry("STRING")
+    AddTextComponentString(txt) -- Main Text string
+    DrawText(x, y)
+    end
+
+    function GetMinimapAnchor()
+        -- Safezone goes from 1.0 (no gap) to 0.9 (5% gap (1/20))
+        -- 0.05 * ((safezone - 0.9) * 10)
+        local safezone = GetSafeZoneSize()
+        local safezone_x = 1.0 / 20.0
+        local safezone_y = 1.0 / 20.0
+        local aspect_ratio = GetAspectRatio(0)
+        local res_x, res_y = GetActiveScreenResolution()
+        local xscale = 1.0 / res_x
+        local yscale = 1.0 / res_y
+        local Minimap = {}
+        Minimap.width = xscale * (res_x / (4 * aspect_ratio))
+        Minimap.height = yscale * (res_y / 5.674)
+        Minimap.left_x = xscale * (res_x * (safezone_x * ((math.abs(safezone - 1.0)) * 10)))
+        Minimap.bottom_y = 1.0 - yscale * (res_y * (safezone_y * ((math.abs(safezone - 1.0)) * 10)))
+        Minimap.right_x = Minimap.left_x + Minimap.width
+        Minimap.top_y = Minimap.bottom_y - Minimap.height
+        Minimap.x = Minimap.left_x
+        Minimap.y = Minimap.top_y
+        Minimap.xunit = xscale
+        Minimap.yunit = yscale
+        return Minimap
+    end
+
+    
+    function conc(args)
+      local subs = {}
+      if #args ~= 0 then
+        if #args > 1  then
+        for k,v in pairs(args) do
+        table.insert(subs, v..' ') 
+        end
+        local str= table.concat(subs)
+                print(str)
+                return str
+        else
+            local str = args[1]
+                    print(str)
+                    return str
+        end
+        end
+
+
+    end
